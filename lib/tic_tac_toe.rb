c92477e2c1de310d6396ea4991e3cfd3f64f39a3
chic-caller-7520 <arbrown110@gmail.com>
2020-04-15 01:46:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,8 +15,10 @@ class TicTacToe
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-
-
+    end
+    
+    def input_to_index
+      
+end
   
  
