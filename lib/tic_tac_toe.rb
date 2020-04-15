c9bc25a8852a724d0d50dc4f3497a7627a5ccf4b
chic-caller-7520 <arbrown110@gmail.com>
2020-04-15 01:40:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,14 @@
 class TicTacToe 
   
-  def intialize 
-    @broad = Array.new(9," ")
-  end  
-  
-  WIN_COMBINATIONS = [
+   WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5]
   ]
+ 
+  def intialize 
+    @broad = Array.new(9," ")
+  end
+  
+  def 
+  
+ 
